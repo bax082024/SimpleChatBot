8f5c7eacdd390ec0d1d369ed307afb8c2e1a0f8d
bax082024 <bax082024@gmail.com>
2024-10-17 22:32:16 +0200

    updated modelInput

--- Model/ModelInput.cs
+++ Model/ModelInput.cs
@@ -1,6 +1,12 @@
+using Microsoft.ML.Data;
+
 public class ModelInput
 {
+  [LoadColumn(0)] //first column of file
   public string? Text { get; set; }
+
+  [LoadColumn(1)] // second column
+  public string Label { get; set; }
 }
 
 public class ModelOutput
